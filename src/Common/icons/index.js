8761079f5b08f71909819867843a2c52e71d0fd8
Chandan <Chandan17ee@gmail.com>
2024-12-13 19:59:54 +0530

    chore: started app

---
+++ src/Common/icons/index.js
@@ -0,0 +1,7 @@
+import Accessibility from './icon-accessibility.svg';
+import CSS from './icon-css.svg';
+import HTML from './icon-html.svg';
+import Javascript from './icon-js.svg';
+import transparent from './transparent.png';
+
+export default {Accessibility, CSS, HTML, Javascript, transparent};
