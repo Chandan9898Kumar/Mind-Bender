a7ea9b33bd1ea04ce05e9f732c7c8222cf884ad0
Chandan <Chandan17ee@gmail.com>
2024-12-22 17:37:14 +0530

    chore: added subject to quiz

--- src/Pages/Quiz/Quiz.js
+++ src/Pages/Quiz/Quiz.js
@@ -1,13 +1,23 @@
 import React, { useState, useCallback, useEffect } from "react";
-
+import styles from "./style.module.css";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import data from "./data";
 const Quiz = () => {
-  const theme = useSelector((state) => state.quiz);
+  const theme = useSelector((state) => state.theme);
+  const subject = useSelector((state) => state.quiz.subject);
+  const currentQuestionNumber = useSelector((state) => state.quiz.currentQuestionNumber);
+  const [allQuestions, setAllQuestions] = useState([]);
+  const [currentQuestion, setCurrentQuestion] = useState(null);
+  const navigate = useNavigate();
 
- 
-  return <div>Quiz</div>;
+  useEffect(() => {
+    if (!subject) {
+      navigate("/");
+    }
+  }, [subject, navigate]);
+
+  return <section className={styles.quiz}>Quiz</section>;
 };
 
 export default Quiz;
